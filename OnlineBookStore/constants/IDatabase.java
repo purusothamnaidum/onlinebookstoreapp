8f694ee3b16e0fc771fd82a717078ea96f01d2ce
kalyanreddyc <kalyanreddychitiki@gmail.com>
2024-05-01 18:17:44 +0530

    adding onlinsestore

---
+++ OnlineBookStore/constants/IDatabase.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class IDatabase {
+
+	public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/onlinebookstore?useSSL=false&serverTimezone=UTC";
+	public static final String USER_NAME = "admin";
+	public static final String PASSWORD = "admin@123";
+
+}
